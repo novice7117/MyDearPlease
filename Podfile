e60f624596dab5ddf9aa5cec8bddfded7d0abdae
novice7117 <vinusubhash117@gmail.com>
2018-06-30 14:48:56 +0530

    My first commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RemoteNotifications1.01' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RemoteNotifications1.01
+pod 'Firebase'
+pod 'Firebase/Messaging'
+end
---
+++ RemoteNotifications1.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteNotifications1.01
+//
+//  Created by CYKUL on 30/06/18.
+//  Copyright © 2018 CYKUL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
