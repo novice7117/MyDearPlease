a3589775f3a3152ca913816345e8da4131120cd1
novice7117 <vinusubhash117@gmail.com>
2018-06-30 18:07:49 +0530

    local notification added

--- RemoteNotifications1.01/ViewController.swift
+++ RemoteNotifications1.01/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
-
+import UserNotifications
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
+    func myNotification()
+    {
+        let content = UNMutableNotificationContent()
+        content.title = "Welcome title"
+        content.body  =  "Body "
+        content.sound = UNNotificationSound.default()
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "Subhash", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 }
 
